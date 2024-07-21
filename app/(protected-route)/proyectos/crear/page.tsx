5df0ef32d9dec3f736a78a63765be3dd13d65817
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-07-21 14:31:14 -0500

    🆕 creacion pagina crear proyecto

---
+++ app/(protected-route)/proyectos/crear/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectCreatePage() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 lg:px-0">
+      <h1 className="text-3xl lg:text-5xl font-extrabold mt-16 text-center">Crear proyecto</h1>
+      
+    </main>
+  )
+}
